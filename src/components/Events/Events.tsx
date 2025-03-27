1b815a49958cba36e5b1f1a4109e9b9feffc83bb
Wang, Liang <wl861213@gmail.com>
2025-03-27 14:45:19 -0700

    Add Google map link for event location
    Signed-off-by: Wang, Liang <wl861213@gmail.com>

--- src/components/Events/Events.tsx
+++ src/components/Events/Events.tsx
@@ -13,11 +13,23 @@ const Events: React.FC = () => {
           <h3 className="event-title">Ceremony</h3>
           <span className="event-time">{'3:30 PM – 4:30 PM'}</span>
         </div>
-        <p className="event-location">
+        <a 
+          href="https://www.google.com/maps/search/?api=1&query=Ellen+Browning+Scripps+Park+La+Jolla"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="event-location"
+        >
           <FaMapMarkerAlt className="location-icon" />
           {'Ellen Browning Scripps Park'}
-        </p>
-        <p className="event-address">{'1100 Coast Blvd, La Jolla, CA 92037'}</p>
+        </a>
+        <a 
+          href="https://www.google.com/maps/search/?api=1&query=1100+Coast+Blvd+La+Jolla+CA+92037"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="event-address"
+        >
+          {'1100 Coast Blvd, La Jolla, CA 92037'}
+        </a>
         <DressCode/>
       </div>
 
@@ -30,11 +42,23 @@ const Events: React.FC = () => {
           <h3 className="event-title">{'Reception'}</h3>
           <span className="event-time">{'6:00 PM – 9:00 PM'}</span>
         </div>
-        <p className="event-location">
+        <a 
+          href="https://www.google.com/maps/search/?api=1&query=Animae+San+Diego"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="event-location"
+        >
           <FaMapMarkerAlt className="location-icon" />
           {'Animae'}
-        </p>
-        <p className="event-address">{'969 Pacific Hwy, San Diego, CA 92101'}</p>
+        </a>
+        <a 
+          href="https://www.google.com/maps/search/?api=1&query=969+Pacific+Hwy+San+Diego+CA+92101"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="event-address"
+        >
+          {'969 Pacific Hwy, San Diego, CA 92101'}
+        </a>
       </div>
     </div>
   )
